d508bcd710466360e3ca25b7214992e17e837bc7
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-18 18:36:05 +0000

    vendor: Allow providing camera server and service
    Change-Id: I6d56930f4673cf7bdf99fccb87c15f0c0fc9a1d7
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: AshutoshSundresh <ashutoshsundresh@gmail.com>

--- config/BoardConfigSsos.mk
+++ config/BoardConfigSsos.mk
@@ -1,2 +1,8 @@
 include vendor/ssos/config/BoardConfigKernel.mk
 include vendor/ssos/config/BoardConfigSoong.mk
+
+ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true)
+PRODUCT_SOONG_NAMESPACES += \
+    frameworks/av/camera/cameraserver \
+    frameworks/av/services/camera/libcameraservice
+endif
