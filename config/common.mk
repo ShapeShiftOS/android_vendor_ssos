459ae4a534d5137e72aef36c7d76104abd64a500
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-06-15 15:49:57 -0300

    vendor: Add permissions for lineagehw
    Change-Id: I55172ac4e9121aa969b628b450781e58151de18c

--- config/common.mk
+++ config/common.mk
@@ -73,7 +73,8 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/ssos/prebuilt/common/etc/permissions/privapp-permissions-ssos-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-ssos-product.xml \
     vendor/ssos/prebuilt/common/etc/permissions/privapp-permissions-ssos-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-ssos-system.xml \
-    vendor/ssos/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-elgoog.xml
+    vendor/ssos/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-elgoog.xml \
+    vendor/ssos/prebuilt/common/etc/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-lineagehw.xml
 
 # system mount
 PRODUCT_COPY_FILES += \
