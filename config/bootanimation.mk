77c5f595be7ec1316d8763b3aa03fdf717ace4b9
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-10-11 22:13:53 +0530

    vendor: Use Pixel boot animation
    Signed-off-by: AshutoshSundresh <ashutoshsundresh@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,6 +1,4 @@
-#
-# Copyright (C) 2018 The Dirty Unicorns Project
-# Copyright (C) 2020 StatiXOS
+# Copyright (C) 2020 ShapeShiftOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# Boot Animation
-PRODUCT_COPY_FILES += \
-    vendor/ssos/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+#Bootanimation
+
+TARGET_BOOT_ANIMATION_RES ?= undefined
+
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_720.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_1440.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),undefined)
+     $(warning Target bootanimation res is undefined, using 1080p bootanimation )
+     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else
+     $(warning Defined bootanimation res is wrong, using 1080p bootanimation )
+     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+endif
