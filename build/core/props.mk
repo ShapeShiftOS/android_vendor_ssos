df8dc635eebdea1a0fc929c8f03f7cdcba23b58e
saikiran2001 <bjsaikiran@gmail.com>
2020-10-11 22:02:51 +0530

    vendor: Add more properties and fixup versioning
    Signed-off-by: AshutoshSundresh <ashutoshsundresh@gmail.com>

--- build/core/props.mk
+++ build/core/props.mk
@@ -7,7 +7,10 @@ endif
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.ssos.version=$(SSOS_BASE_VERSION)-$(SSOS_BUILD_TYPE)-$(BUILD_DATE)-$(BUILD_TIME) \
     ro.ssos.base.version=$(SSOS_BASE_VERSION) \
+    ro.ssos.codename=$(SSOS_CODENAME) \
+    ro.ssos.build.version=$(SSOS_PLATFORM_VERSION) \
     ro.mod.version=$(BUILD_ID)-$(BUILD_DATE)-$(SSOS_BASE_VERSION) \
     ro.ssos.fingerprint=$(ROM_FINGERPRINT) \
     ro.ssos.buildtype=$(SSOS_BUILD_TYPE) \
-    ro.ssos.device=$(TARGET_PRODUCT)
+    ro.ssos.device=$(TARGET_PRODUCT) \
+    ro.ssos.build_date=$(BUILD_DATE)
