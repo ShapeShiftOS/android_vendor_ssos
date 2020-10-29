b59dcc5fc30940ee0368c196041724e67fee9d84
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-10-29 20:21:27 +0530

    Drop Inter font package from Revenge
    This reverts commit d258ea9f3d680a34986e3a3e7365f6c60622e0a9.

--- config/overlay.mk
+++ config/overlay.mk
@@ -31,7 +31,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FontArbutusSourceOverlay \
     FontArvoLatoOverlay \
-    FontInterOverlay \
     FontRubikRubikOverlay \
     FontGoogleSansOverlay \
     FontOnePlusSlateOverlay \
