a42dc703f2d439b6e7bde0e59bd42b2feefb9f6b
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-11-18 14:12:05 +0530

    ssos: Make Arbutus Slab, Inter and Rubik the only fonts available

--- config/fonts.mk
+++ config/fonts.mk
@@ -39,15 +39,6 @@ PRODUCT_PACKAGES += \
     Inter-Medium.otf \
     Inter-SemiBoldItalic.otf
 
-# lato family
-PRODUCT_PACKAGES += \
-    Lato-BoldItalic.ttf \
-    Lato-Bold.ttf \
-    Lato-Italic.ttf \
-    Lato-MediumItalic.ttf \
-    Lato-Medium.ttf \
-    Lato-Regular.ttf
-
 # rubik family
 PRODUCT_PACKAGES += \
     Rubik-BoldItalic.ttf \
@@ -57,20 +48,10 @@ PRODUCT_PACKAGES += \
     Rubik-Medium.ttf \
     Rubik-Regular.ttf
 
-# zilla-slab family
-PRODUCT_PACKAGES += \
-    ZillaSlab-MediumItalic.ttf \
-    ZillaSlab-Medium.ttf \
-    ZillaSlab-SemiBoldItalic.ttf \
-    ZillaSlab-SemiBold.ttf
-
 # Customization overlays
 PRODUCT_PACKAGES += \
     FontArbutusSourceOverlay \
-    FontArvoLatoOverlay \
-    FontRubikRubikOverlay \
-    FontSlateForOnePlusOverlay \
-    FontSanFranciscoOverlay
+    FontRubikRubikOverlay
 
 PRODUCT_COPY_FILES += \
     vendor/ssos/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
