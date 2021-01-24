e5aa1f91230507777deb4b2e4bcd84ab49b23e18
Alexandru Scurtu <sasha.scurtu@gmail.com>
2021-01-24 11:52:56 +0200

    roomservice: adapt for ShapeShiftOS

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -42,15 +42,15 @@ except ImportError:
 
 DEBUG = False
 
-custom_local_manifest = ".repo/local_manifests/fluid.xml"
-custom_default_revision =  os.getenv('ROOMSERVICE_DEFAULT_BRANCH', 'fluid-11')
-custom_dependencies = "fluid.dependencies"
-org_manifest = "fluid-devices"  # leave empty if org is provided in manifest
-org_display = "Project-Fluid-Devices"  # needed for displaying
+custom_local_manifest = ".repo/local_manifests/ssos.xml"
+custom_default_revision =  os.getenv('ROOMSERVICE_DEFAULT_BRANCH', 'android_11')
+custom_dependencies = "ssos.dependencies"
+org_manifest = "ShapeShiftOS-Devices"  # leave empty if org is provided in manifest
+org_display = "ShapeShiftOS-Devices"  # needed for displaying
 
 default_manifest = ".repo/manifests/default.xml"
-fluid_manifest = ".repo/manifests/snippets/fluid.xml"
-lineage_manifest = ".repo/manifests/snippets/lineage.xml"
+ssos_manifest = ".repo/manifests/extras/ssos.xml"
+external_manifest = ".repo/manifests/extras/external.xml"
 
 github_auth = os.getenv('GITHUB_API_TOKEN', None)
 
@@ -134,8 +134,8 @@ def is_in_manifest(project_path):
 def add_to_manifest(repos, fallback_branch=None):
     lm = load_manifest(custom_local_manifest)
     mlm = load_manifest(default_manifest)
-    fluidm = load_manifest(fluid_manifest)
-    lineagem = load_manifest(lineage_manifest)
+    ssosm = load_manifest(ssos_manifest)
+    externalm = load_manifest(external_manifest)
 
     for repo in repos:
         repo_name = repo['repository']
@@ -158,10 +158,10 @@ def add_to_manifest(repos, fallback_branch=None):
         existing_m_project = None
         if exists_in_tree(mlm, repo_path) != None:
            existing_m_project = exists_in_tree(mlm, repo_path)
-        elif exists_in_tree(fluidm, repo_path) != None:
-             existing_m_project = exists_in_tree(fluidm, repo_path)
-        elif exists_in_tree(lineagem, repo_path) != None:
-             existing_m_project = exists_in_tree(lineagem, repo_path)
+        elif exists_in_tree(ssosm, repo_path) != None:
+             existing_m_project = exists_in_tree(ssosm, repo_path)
+        elif exists_in_tree(externalm, repo_path) != None:
+             existing_m_project = exists_in_tree(externalm, repo_path)
 
         if existing_m_project != None:
             if existing_m_project.attrib['path'] == repo['target_path']:
