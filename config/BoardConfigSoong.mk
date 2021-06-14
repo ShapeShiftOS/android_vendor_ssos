261c8523e085622e3e783bd9235476ca91ffea43
Bruno Martins <bgcngm@gmail.com>
2021-06-14 14:47:33 +0100

    Revert "soong: Add TARGET_QTI_USB_SUPPORTS_{AUDIO,DEBUG}_ACCESSORY flags"
    This reverts commit 44f5c2a6ad35b94dbc6df7d78d50af1e90a0ffe7.
    It is no longer necessary.
    
    Change-Id: I5d9656ffb686e939f006d5fb1effaba835f93ed2

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -56,8 +56,6 @@ SOONG_CONFIG_NAMESPACES += ssosQcomVars
 SOONG_CONFIG_ssosQcomVars += \
     legacy_hw_disk_encryption \
     no_camera_smooth_apis \
-    supports_audio_accessory \
-    supports_debug_accessory \
     should_wait_for_qsee \
     supports_extended_compress_format \
     supports_hw_fde \
@@ -81,8 +79,6 @@ SOONG_CONFIG_ssosGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_ssosGlobalVars_target_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_ssosNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_ssosQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
-SOONG_CONFIG_ssosQcomVars_supports_audio_accessory := $(TARGET_QTI_USB_SUPPORTS_AUDIO_ACCESSORY)
-SOONG_CONFIG_ssosQcomVars_supports_debug_accessory := $(TARGET_QTI_USB_SUPPORTS_DEBUG_ACCESSORY)
 SOONG_CONFIG_ssosQcomVars_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_QSEE)
 SOONG_CONFIG_ssosQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_ssosQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
