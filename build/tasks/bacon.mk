fc06e60ee4bb6e14e4af918345965c0c8b37b12c
xawlw <abdulazizawlw@gmail.com>
2021-10-16 11:42:16 +0200

    ssos: Change MD5 to SHA256
    https://github.com/LineageOS/android_vendor_lineage/commit/cb30d255fbbb76bed0caeade36257a3d461ef4f3
    * Now we generate SHA256 instead of MD5

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -45,7 +45,7 @@ bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	@echo -e ""
 	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
 	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(SSOS_TARGET_PACKAGE)${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"MD5: "${CL_RED}" `cat $(SSOS_TARGET_PACKAGE).md5sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"SHA256: "${CL_RED}" `cat $(SSOS_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(SSOS_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_GRN}"TimeStamp:"${CL_RED}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
 	echo -e ${CL_BLD}${CL_GRN}"Integer Value:"${CL_RED}" `wc -c $(SSOS_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
