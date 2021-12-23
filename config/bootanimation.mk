9e7d25c6475be0ce256ae52d658038f5393904de
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-23 17:25:41 +0530

    ssos: Move bootanimation to product
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Signed-off-by: palaych <palaychm@yandex.ru>
    Change-Id: I9972653845c64aab944d0289d9357361e2feb344

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -17,15 +17,15 @@
 TARGET_BOOT_ANIMATION_RES ?= undefined
 
 ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_720.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_1440.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),undefined)
      $(warning Target bootanimation res is undefined, using 1080p bootanimation )
-     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else
      $(warning Defined bootanimation res is wrong, using 1080p bootanimation )
-     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/ssos/bootanimation/bootanimation-dark_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
