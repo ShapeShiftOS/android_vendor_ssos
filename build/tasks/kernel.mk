d95bd9ee0c5fc859967717aa9b5452ebb218d352
dianlujitao <dianlujitao@lineageos.org>
2022-06-12 16:42:37 +0800

    kernel: Clean up kernel copy recipes
    ACP and ALL_PREBUILT are dropped years ago and thus have become no-ops.
    
    Change-Id: Ic317cb4a4fa406cce0dc86555fb8da34d21fb2bf

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -503,12 +503,8 @@ endif
 ## Install it
 
 ifeq ($(NEEDS_KERNEL_COPY),true)
-file := $(INSTALLED_KERNEL_TARGET)
-ALL_PREBUILT += $(file)
-$(file) : $(KERNEL_BIN) | $(ACP)
+$(INSTALLED_KERNEL_TARGET): $(KERNEL_BIN)
 	$(transform-prebuilt-to-target)
-
-ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 endif
 
 ifeq ($(RECOVERY_KERNEL_COPY),true)
